ede88a46abc773d0e20c5228fcd2a4d8bef52126
Joshua Beverly <beverlyj93@hotmail.com>
2019-03-28 18:38:54 -0500

    Changes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var connection = require('./config/connection');
 var app = express();
 var port = process.env.port || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+connection.connect(err => {
+    if(err) throw err;
+    console.log("Connected to database.");
+})
 
 app.get('/', (req, res) => {
     // Send homepage
