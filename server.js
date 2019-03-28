a5dedf1ac6117a8cc63f92c0fb132daa34eb280f
Joshua Beverly <beverlyj93@hotmail.com>
2019-03-28 18:25:17 -0500

    Express

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var port = process.env.port || 3000;
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     // Send homepage
